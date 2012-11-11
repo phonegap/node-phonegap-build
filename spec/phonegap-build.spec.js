941e8b1295ecd32bd6831ce83311c9cd97302051
Michael Brooks <michael@michaelbrooks.ca>
2012-11-11 14:52:54 -0800

    [lib] Stub User and App modules.

--- spec/phonegap-build.spec.js
+++ spec/phonegap-build.spec.js
@@ -2,8 +2,20 @@ var phonegap = require('../lib/phonegap-build');
 
 describe('phonegap-build', function() {
     describe('login', function() {
-        it('should exist', function() {
+        it('should be a function', function() {
             expect(phonegap.login).toEqual(jasmine.any(Function));
+        });
+    });
+
+    describe('logout', function() {
+        it('should be a function', function() {
+            expect(phonegap.logout).toEqual(jasmine.any(Function));
+        });
+    });
+
+    describe('app', function() {
+        it('should be an object', function() {
+            expect(phonegap.app).toEqual(jasmine.any(Function));
         });
     });
 });
