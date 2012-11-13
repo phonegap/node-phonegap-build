a690796a06a240ea065c7a25c6ff3fcd6d4784a0
Michael Brooks <michael@michaelbrooks.ca>
2012-11-13 14:44:27 -0800

    [spec] Add CLI version spec.

--- spec/cli.spec.js
+++ spec/cli.spec.js
@@ -1,72 +1,16 @@
 var CLI = require('../lib/cli'),
     cli;
 
-describe('$', function() {
+describe('CLI', function() {
     beforeEach(function() {
         cli = new CLI();
     });
 
-    describe('$ phonegap-build', function() {
-        it('should display help information', function() {
-            spyOn(cli, 'help');
-            cli.parse({ _: [] });
-            expect(cli.help).toHaveBeenCalled();
-        });
-    });
-
-    describe('$ phonegap-build help', function() {
-        it('should display help information', function() {
-            spyOn(cli, 'help');
-            cli.parse({ _: [ 'help' ] });
-            expect(cli.help).toHaveBeenCalled();
-        });
-    });
-
-    describe('$ phonegap-build --help', function() {
-        it('should be display help information', function() {
-            spyOn(cli, 'help');
-            cli.parse({ _: [], help: true });
-            expect(cli.help).toHaveBeenCalled();
-        });
-    });
-
-    describe('$ phonegap-build -h', function() {
-        it('should be display help information', function() {
-            spyOn(cli, 'help');
-            cli.parse({ _: [], h: true });
-            expect(cli.help).toHaveBeenCalled();
-        });
-    });
-
-    describe('$ phonegap-build --version', function() {
-        it('should display version', function() {
-            spyOn(cli, 'version');
-            cli.parse({ _: [], version: true });
-            expect(cli.version).toHaveBeenCalled();
-        });
-    });
-
-    describe('$ phonegap-build -v', function() {
-        it('should display version', function() {
-            spyOn(cli, 'version');
-            cli.parse({ _: [], v: true });
-            expect(cli.version).toHaveBeenCalled();
-        });
-    });
-
-    describe('$ phonegap-build login', function() {
-        it('should call login action', function() {
-            spyOn(cli, 'login');
-            cli.parse({ _: ['login'] });
-            expect(cli.login).toHaveBeenCalled();
-        });
-    });
-
-    describe('$ phonegap-build noop', function() {
-        it('should display unknown command', function() {
-            spyOn(console, 'log');
-            cli.parse({ _: [ 'noop' ] });
-            expect(console.log.mostRecentCall.args[0]).toMatch(/unknown command:/i);
+    describe('version', function() {
+        it('should output with the format x.x.x', function() {
+            spyOn(process.stdout, 'write');
+            cli.version();
+            expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/\d+\.\d+\.\d+/);
         });
     });
 });
