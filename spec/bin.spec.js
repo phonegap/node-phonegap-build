b352bbd0eefacf48eab1c70f873faab975b62e2c
Michael Brooks <michael@michaelbrooks.ca>
2012-11-12 22:01:33 -0800

    [spec] Rename cli.spec to bin.spec

--- spec/bin.spec.js
+++ spec/bin.spec.js
@@ -2,7 +2,7 @@ var shell = require('shelljs');
 var path = require('path');
 var cli = 'node ' + path.resolve(path.join(__dirname, '..', 'bin', 'phonegap-build.js'));
 
-describe('Command-line', function() {
+describe('$', function() {
     describe('$ phonegap-build', function() {
         it('should display help information', function() {
             var process = shell.exec(cli + '', { silent: true });
