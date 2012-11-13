c0ab04b968fc2c02040b0c6f20f1605a02b636bf
Michael Brooks <michael@michaelbrooks.ca>
2012-11-12 21:49:30 -0800

    [cli] Move argument parsing to testable module.

--- bin/phonegap-build.js
+++ bin/phonegap-build.js
@@ -6,29 +6,9 @@
 
 var argv = require('optimist').argv;
 var CLI = require('../lib/phonegap-build-cli');
-var cli = new CLI();
 
 /*
- * Parse the arguments
+ * Run the command-line client
  */
 
-if (argv.version || argv.v) {
-    cli.version();
-    process.exit();
-}
-
-if (argv.help || argv.h) {
-    cli.help();
-    process.exit();
-}
-
-if (!argv._.length) {
-    cli.help();
-    process.exit();
-}
-
-try {
-    cli[argv._[0]]();
-} catch(e) {
-    console.log('Unknown command:', argv._.join(' '));
-}
+var cli = new CLI().parse(argv);
