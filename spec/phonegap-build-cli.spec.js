e13e21209d958f44a0a9f7ddeb0f21d259c6b3f2
Michael Brooks <michael@michaelbrooks.ca>
2012-11-12 23:10:50 -0800

    [spec] Add phonegap-build-cli specs.

---
+++ spec/phonegap-build-cli.spec.js
@@ -0,0 +1,64 @@
+var CLI = require('../lib/phonegap-build-cli'),
+    cli;
+
+describe('CLI', function() {
+    beforeEach(function() {
+        cli = new CLI();
+    });
+
+    describe('no arguments', function() {
+        it('should display help information', function() {
+            spyOn(cli, 'help');
+            cli.parse({ _: [] });
+            expect(cli.help).toHaveBeenCalled();
+        });
+    });
+
+    describe('$ phonegap-build help', function() {
+        it('should display help information', function() {
+            spyOn(cli, 'help');
+            cli.parse({ _: [ 'help' ] });
+            expect(cli.help).toHaveBeenCalled();
+        });
+    });
+
+    describe('$ phonegap-build --help', function() {
+        it('should be display help information', function() {
+            spyOn(cli, 'help');
+            cli.parse({ _: [], help: true });
+            expect(cli.help).toHaveBeenCalled();
+        });
+    });
+
+    describe('$ phonegap-build -h', function() {
+        it('should be display help information', function() {
+            spyOn(cli, 'help');
+            cli.parse({ _: [], h: true });
+            expect(cli.help).toHaveBeenCalled();
+        });
+    });
+
+    describe('$ phonegap-build --version', function() {
+        it('should display version', function() {
+            spyOn(cli, 'version');
+            cli.parse({ _: [], version: true });
+            expect(cli.version).toHaveBeenCalled();
+        });
+    });
+
+    describe('$ phonegap-build -v', function() {
+        it('should display version', function() {
+            spyOn(cli, 'version');
+            cli.parse({ _: [], v: true });
+            expect(cli.version).toHaveBeenCalled();
+        });
+    });
+
+    describe('$ phonegap-build noop', function() {
+        it('should display unknown command', function() {
+            spyOn(console, 'log');
+            cli.parse({ _: [ 'noop' ] });
+            expect(console.log.mostRecentCall.args[0]).toMatch(/unknown command:/i);
+        });
+    });
+});
