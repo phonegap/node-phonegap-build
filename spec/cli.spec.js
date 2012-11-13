e9e3354e257d959287ec02874fe0dde95b02b2b0
Michael Brooks <michael@michaelbrooks.ca>
2012-11-13 15:25:06 -0800

    [lib] Add CLI login prompt.

--- spec/cli.spec.js
+++ spec/cli.spec.js
@@ -1,4 +1,6 @@
-var CLI = require('../lib/cli'),
+var prompt = require('prompt'),
+    client = require('phonegap-build-rest'),
+    CLI = require('../lib/cli'),
     cli;
 
 describe('CLI', function() {
@@ -19,6 +21,31 @@ describe('CLI', function() {
             spyOn(process.stdout, 'write');
             cli.help();
             expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/usage:/i);
+        });
+    });
+
+    describe('login', function() {
+        beforeEach(function() {
+            spyOn(prompt, 'get').andCallFake(function(options, callback) {
+                callback(null, { username: 'zelda', password: 'tr1force' });
+            });
+        });
+
+        describe('prompt', function() {
+            it('should be called when not logged in', function() {
+                spyOn(client, 'auth');
+                cli.login();
+                expect(prompt.get).toHaveBeenCalled();
+            });
+
+            it('should pass information to login', function() {
+                spyOn(client, 'auth');
+                cli.login();
+                expect(client.auth).toHaveBeenCalledWith(
+                    { username: 'zelda', password: 'tr1force' },
+                    jasmine.any(Function)
+                );
+            });
         });
     });
 });
