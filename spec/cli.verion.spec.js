ae1d3b5f3c3926d7318de8922fa94107a373633d
Michael Brooks <michael@michaelbrooks.ca>
2012-11-21 14:49:48 -0800

    [cli] Test that version is included in help dialog.

--- spec/cli.verion.spec.js
+++ spec/cli.verion.spec.js
@@ -7,6 +7,14 @@ describe('command-line version', function() {
         spyOn(process.stdout, 'write');
     });
 
+    describe('$ phonegap-build help', function() {
+        it('outputs info on the version command', function() {
+            cli.argv({ _: [ 'help' ] });
+            expect(process.stdout.write.mostRecentCall.args[0])
+                .toMatch(/Commands:[\w\W]*\s+version/i);
+        });
+    });
+
     describe('$ phonegap-build --version', function() {
         it('should output with the format x.x.x', function() {
             cli.argv({ _: [], version: true });
