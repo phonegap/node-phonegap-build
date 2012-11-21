95d434a48c622a912fc28f046f02f7d1d60dbde6
Michael Brooks <michael@michaelbrooks.ca>
2012-11-21 15:33:06 -0800

    [cli] Implement create help command.

--- spec/cli.create.spec.js
+++ spec/cli.create.spec.js
@@ -1,10 +1,12 @@
 var CLI = require('../lib/cli'),
-    cli;
+    cli,
+    stdout;
 
 describe('command-line create', function() {
     beforeEach(function() {
         cli = new CLI();
         spyOn(process.stdout, 'write');
+        stdout = process.stdout.write;
     });
 
     describe('$ phonegap-build help', function() {
@@ -16,15 +18,32 @@ describe('command-line create', function() {
     });
 
     describe('$ phonegap-build create', function() {
+        // Usage: phonegap-build.js create <path> [options]
+        it('should output the create help dialog', function() {
+            cli.argv({ _: ['create'] });
+            expect(stdout.mostRecentCall.args[0]).toMatch(/usage: [\S]+ create/i);
+        });
     });
 
     describe('$ phonegap-build create help', function() {
+        it('should output the create help dialog', function() {
+            cli.argv({ _: ['create'] });
+            expect(stdout.mostRecentCall.args[0]).toMatch(/usage: [\S]+ create/i);
+        });
     });
 
     describe('$ phonegap-build create --help', function() {
+        it('should output the create help dialog', function() {
+            cli.argv({ _: ['create'] });
+            expect(stdout.mostRecentCall.args[0]).toMatch(/usage: [\S]+ create/i);
+        });
     });
 
     describe('$ phonegap-build create -h', function() {
+        it('should output the create help dialog', function() {
+            cli.argv({ _: ['create'] });
+            expect(stdout.mostRecentCall.args[0]).toMatch(/usage: [\S]+ create/i);
+        });
     });
 
     describe('$ phonegap-build create ./my-app', function() {
