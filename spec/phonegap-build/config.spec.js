4e945f5b44765c308c818e2ce13ba01a7ca46751
Michael Brooks <michael@michaelbrooks.ca>
2012-12-03 22:47:16 -0800

    [lib] Implement config.save

--- spec/phonegap-build/config.spec.js
+++ spec/phonegap-build/config.spec.js
@@ -4,7 +4,8 @@
 
 var config = require('../../lib/phonegap-build/config'),
     path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    data;
 
 /*
  * Specification for config.
@@ -154,5 +155,100 @@ describe('config', function() {
     });
 
     describe('config.save(data, callback)', function() {
+        beforeEach(function() {
+            spyOn(config, 'load');
+            data = { token: 'abc123' };
+        });
+
+        it('should require the data parameter', function() {
+            expect(function() { config.save(); }).toThrow();
+        });
+
+        it('should require the callback parameter', function() {
+            expect(function() { config.save(data); }).toThrow();
+        });
+
+        it('should try to load the config file', function() {
+            config.save(data, function(e) {});
+            expect(config.load).toHaveBeenCalled();
+        });
+
+        describe('successful load', function() {
+            beforeEach(function() {
+                config.load.andCallFake(function(callback) {
+                    callback(null, { token: 'abc123' });
+                });
+            });
+
+            it('should try to write to config file', function() {
+                spyOn(fs, 'writeFile');
+                config.save({}, function(e) {});
+                expect(fs.writeFile).toHaveBeenCalled();
+                expect(fs.writeFile.mostRecentCall.args[0]).toEqual(
+                    path.join(config.path, 'config.json')
+                );
+            });
+
+            describe('successful write', function() {
+                beforeEach(function() {
+                    spyOn(fs, 'writeFile').andCallFake(function(filepath, data, callback) {
+                        callback(null);
+                    });
+                });
+
+                it('should support appending keys', function(done) {
+                    config.save({ username: 'link' }, function(e) {
+                        var data = JSON.parse(fs.writeFile.mostRecentCall.args[1]);
+                        expect(data).toEqual({ token: 'abc123', username: 'link' });
+                        done();
+                    });
+                });
+
+                it('should support replacing keys', function(done) {
+                    config.save({ token: 'def456' }, function(e) {
+                        var data = JSON.parse(fs.writeFile.mostRecentCall.args[1]);
+                        expect(data).toEqual({ token: 'def456' });
+                        done();
+                    });
+                });
+
+                it('should trigger callback without an error', function(done) {
+                    config.save({ token: 'def456' }, function(e) {
+                        expect(e).toBeNull();
+                        done();
+                    });
+                });
+            });
+
+            describe('failed write', function() {
+                beforeEach(function() {
+                    spyOn(fs, 'writeFile').andCallFake(function(filepath, data, callback) {
+                        callback(new Error('no write access'));
+                    });
+                });
+
+                it('should trigger callback with an error', function(done) {
+                    config.save({ token: 'def456' }, function(e) {
+                        expect(e).toEqual(jasmine.any(Error));
+                        done();
+                    });
+                });
+            });
+        });
+
+        describe('failed load', function() {
+            beforeEach(function() {
+                config.load.andCallFake(function(callback) {
+                    callback(new Error('no write access'));
+                });
+            });
+
+            it('should trigger callback with an error', function(done) {
+                config.save(data, function(e) {
+                    expect(e).toEqual(jasmine.any(Error));
+                    done();
+                });
+            });
+        });
     });
 });
