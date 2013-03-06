60779abb946947534e97bb0278126999394cf6d0
Michael Brooks <michael@michaelbrooks.ca>
2013-03-05 17:03:01 -0800

    [cli] Update comment formatting in bin/

--- bin/phonegap-build.js
+++ bin/phonegap-build.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-/*
+/*!
  * Module dependencies.
  */
 
 var argv = require('optimist').argv;
 var CLI = require('../lib/cli');
 
-/*
- * Run the command-line client
+/*!
+ * Run the command-line client.
  */
 
 var cli = new CLI().argv(argv);
