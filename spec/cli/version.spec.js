a3869e976a7e535b5a6ad52fcc6eb06a35fcf7c0
Michael Brooks <michael@michaelbrooks.ca>
2013-03-06 15:47:35 -0800

    [cli] Add version command.

--- spec/cli/version.spec.js
+++ spec/cli/version.spec.js
@@ -17,9 +17,16 @@ describe('$ phonegap-build --version', function() {
 
     describe('$ phonegap-build help', function() {
         it('outputs info on the version command', function() {
-            cli.argv({ _: [ 'help' ] });
+            cli.argv({ _: ['help'] });
             expect(process.stdout.write.mostRecentCall.args[0])
                 .toMatch(/Commands:[\w\W]*\s+version/i);
+        });
+    });
+
+    describe('$ phonegap-build version', function() {
+        it('should output with the format x.x.x', function() {
+            cli.argv({ _: ['version'] });
+            expect(process.stdout.write.mostRecentCall.args[0]).toMatch(/\d+\.\d+\.\d+/);
         });
     });
 
