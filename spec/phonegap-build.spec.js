f3c1c5b9408c0616a838c9973d777403e9c4e1a0
Michael Brooks <michael@michaelbrooks.ca>
2013-04-03 13:57:58 -0700

    [#34] Add global event support.

--- spec/phonegap-build.spec.js
+++ spec/phonegap-build.spec.js
@@ -3,13 +3,17 @@
  */
 
 var PhoneGapBuild = require('../lib/phonegap-build'),
-    phonegapbuild = new PhoneGapBuild();
+    phonegapbuild;
 
 /*!
  * PhoneGapBuild specification.
  */
 
 describe('phonegapbuild', function() {
+    beforeEach(function() {
+        phonegapbuild = new PhoneGapBuild();
+    });
+
     it('should have a login action', function() {
         expect(phonegapbuild.login).toEqual(jasmine.any(Function));
     });
@@ -24,5 +28,21 @@ describe('phonegapbuild', function() {
 
     it('should have a build action', function() {
         expect(phonegapbuild.build).toEqual(jasmine.any(Function));
+    });
+
+    it('should support global events', function(done) {
+        phonegapbuild.on('log', function() {
+            done();
+        });
+        phonegapbuild.emit('log', 'hello world');
+    });
+
+    it('should not throw "error" events', function(done) {
+        phonegapbuild.on('error', function() {
+            done();
+        });
+        expect(function() {
+            phonegapbuild.emit('error', new Error('hello world'));
+        }).not.toThrow();
     });
 });
