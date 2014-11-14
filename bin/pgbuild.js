587c9801968f6a490f1ac6e5b45cd7940e5f2d9b
Lorin Beer <lorin.beer@gmail.com>
2014-11-14 12:47:14 -0800

    [#46] bin dir and implementation for handling command line interactions

---
+++ bin/pgbuild.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+/*!
+ * Module dependencies.
+ */
+
+var cli = require('../lib/cli/argv');
+
+/*!
+ * Run the command-line client.
+ */
+cli(process.argv, function(e) {
+    if (e) {
+        process.exit(e.exitCode || 1);
+    }
+});
