c949ee6ea62e8229ef83be6cdf2e1143534a7fa8
Lorin Beer <lorin.beer@gmail.com>
2014-11-18 02:01:41 -0800

    [#46] first pass cli/build module spec updated to jasmine 1.3.1, current spec still behaves > difficulty in silencing console.prompt, tests behave as they should but unwanted output

---
+++ spec/cli/build.spec.js
@@ -0,0 +1,55 @@
+var build = require('../../lib/cli/remote.build.js');
+var pgbuild = require('../../lib/main');
+var console = require('../../lib/cli/console');
+var login = require('../../lib/cli/remote.login');
+
+describe("", function () {
+    var callback = jasmine.createSpy();;
+
+    beforeEach(function() {
+        login = jasmine.createSpy('login').andCallFake(function(options, callback){
+            callback(null, {});
+        });
+        
+        spyOn(console, 'prompt').andCallFake(function(options, callback) {
+            callback(null, {});
+        });
+   
+        spyOn(pgbuild, 'build');
+ 
+    });
+
+
+    describe("pgbuild build command", function () {
+
+        beforeEach(function () {});
+
+        it("should call login cli module", function () {
+            build({_:['build', 'android']}, function () {
+                expect(login).toHaveBeenCalled();
+            });
+        });
+
+        it("should call prompt module if not logged in", function () {
+            build({_:['build', 'android']}, function () {
+                expect(console.prompt).toHaveBeenCalled();
+            });
+        });
+
+        it("should call prompt module if not logged in", function () {
+            build({_:['build', 'android']}, function () {
+                expect(pgbuild.build).toHaveBeenCalled();
+            });
+        });
+
+    });
+
+    describe("", function () {
+
+    });
+
+    describe("", function () {
+
+    });
+
+});
