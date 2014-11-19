33e72d4c679f59b945bec3023a4ec6f4160c912b
Lorin Beer <lorin.beer@gmail.com>
2014-11-19 09:35:49 -0800

    [#49] expanded test suite
    properly tests asynch functions
    most of the behaviour is centered around login

--- spec/cli/build.spec.js
+++ spec/cli/build.spec.js
@@ -9,6 +9,7 @@ var build = rewire('../../lib/cli/remote.build.js');
 describe("", function () {
     var callback = jasmine.createSpy();
     var login;
+    var flag;
  
     beforeEach(function() {
 
@@ -17,56 +18,123 @@ describe("", function () {
         });
         build.__set__('login', loginspy);
 
-        spyOn(console, 'prompt').andCallFake(function(options, callback) {
+        prompt = spyOn(console, 'prompt').andCallFake(function(options, callback) {
             callback(null, {});
         });
    
-        spyOn(pgbuild, 'build');
+        spyOn(pgbuild, 'build').andCallFake(function(options, callback) {
+            callback(null, {});
+        });
 
         spyOn(pgbuild, 'login');
+
+        flag = false;
     });
 
 
     describe("pgbuild build command", function () {
+
         it("should call login cli module", function () {
-            build({_:['build', 'android']}, function () {
-                expect(login).toHaveBeenCalled();
+            runs(function(){
+                build({_:['build', 'android']}, function () {
+                    flag = true;
+                });
+            });
+
+            waitsFor(function(){
+                return flag;
+            }, "", 5000);
+
+            runs(function(){
+                expect(loginspy).toHaveBeenCalled();
             });
         });
 
         it("should call prompt module if not logged in", function () {
-            build({_:['build', 'android']}, function () {
-                expect(console.prompt).toHaveBeenCalled();
+            runs(function(){
+                build({_:['build', 'android']}, function () {
+                    flag = true;
+                });
             });
+
+             waitsFor(function(){
+                return flag;
+            }, "", 5000);
+
+            runs(function(){
+                expect(console.prompt).not.toHaveBeenCalled();
+            });
+            
         });
 
         it("should call main build", function () {
-            build({_:['build', 'android']}, function () {
+            runs(function () {
+                build({_:['build', 'android']}, function () {
+                    flag = true;
+                });
+            });
+
+            waitsFor(function() {
+                return flag;
+            }, "", 5000);
+
+            runs(function() {
+                expect(flag).toBe(true);
                 expect(pgbuild.build).toHaveBeenCalled();
             });
         });
     });
 
-    describe("build with failed login", function () {
+
+    describe("if not logged in", function () {
+        var flag;
+
         beforeEach(function() {
+            var flag = false;
+
+            build.__get__('login').reset();
+            
             loginspy = jasmine.createSpy().andCallFake(function(options, callback) {
-                callback(new Error("login failue"));
+                callback(new Error("login failure"));
             });
+
             build.__set__('login', loginspy);
         });
 
-        it("should", function () {
 
-            build({_:['build', 'android']}, function (error, result) {
-                expect(login).toHaveBeenCalled();
+        it("should prompt for login", function () {
+            runs(function(){
+                 build({_:['build', 'android']}, function (error, result) {
+                    flag = true;                          
+                });
             });
 
-        }); 
-*/
+            waitsFor(function(){
+                return flag
+            }, "", 5000);
+
+            runs(function (){
+                expect(prompt).toHaveBeenCalled();
+                expect(prompt.callCount).toEqual(1);
+            });
+        });
+
+
+        it("should execute build when successfully logged in after prompt", function () {
+            runs(function(){
+                build({_:['build', 'android']}, function (error, result) {
+                    flag = true;                          
+                });
+            });
+
+            waitsFor(function(){
+                return flag;
+            }, "callback to be executed", 5000);
+
+            runs(function(){
+                expect(loginspy).toHaveBeenCalled();
+                expect(loginspy.callCount).toEqual(2);
+            });
+       });
     });
-
-    describe("", function () {
-
-    });
-
 });
